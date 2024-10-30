e106936e39edc298fc9c833101b3d0b2bb8f8a46
varad-pawar1 <varadpawarm@gmail.com>
2024-10-30 11:38:57 +0530

    Add website assets, css, and js . website is completed

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # portfolio
 varad pawar portfolio
+
+
+
+Deployed link :- https://varadpawarportfolio.netlify.app/
---
+++ js/script.js
@@ -0,0 +1,27 @@
+// Get elements
+const menuToggle = document.getElementById('menu-toggle');
+const navLinks = document.getElementById('nav-links');
+const navLinksItems = navLinks.querySelectorAll('li a'); // Select all anchor links in the nav
+
+// Toggle the menu on click
+menuToggle.addEventListener('click', (e) => {
+  navLinks.classList.toggle('active');
+  menuToggle.classList.toggle('active');
+  e.stopPropagation(); // Prevents the click event from bubbling up to the document
+});
+
+// Close the menu when clicking outside of it
+document.addEventListener('click', (e) => {
+  if (navLinks.classList.contains('active') && !navLinks.contains(e.target) && !menuToggle.contains(e.target)) {
+    navLinks.classList.remove('active');
+    menuToggle.classList.remove('active');
+  }
+});
+
+// Close the menu when clicking on a nav link
+navLinksItems.forEach(link => {
+  link.addEventListener('click', () => {
+    navLinks.classList.remove('active');
+    menuToggle.classList.remove('active');
+  });
+});
